278b8384bf9e9b4ee32cf68eb2b5de80ed859e7f
huakyouin <jxh0706@126.com>
2024-06-16 19:24:11 +0800

    update

--- utils/news_collecting.py
+++ utils/news_collecting.py
@@ -1,16 +1,15 @@
-# Note: selenium>4.6, or you need to install webdriver on your own
+## Note selenium>4.6, otherwise need to install webdirver yourself
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from pathos.multiprocessing import ProcessPool
 from datetime import datetime
-import os
-import re
+from pathos.multiprocessing import ProcessPool
 import pandas as pd
+import os
 import numpy as np
-
 
 # 设置无头模式
 chrome_options = Options()
@@ -18,18 +17,21 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--log-level=3')
 
 
 def get_news_details(driver,url):
     driver.get(url)
     title = driver.find_element(By.CSS_SELECTOR,".newstitle").text.strip()
     time = driver.find_element(By.CSS_SELECTOR,".newsauthor .time").text.strip()
-    content = " ".join([p.text.strip() for p in driver.find_elements(By.CSS_SELECTOR,".newstext p")])
+    # content = " ".join([p.text.strip() for p in driver.find_elements(By.CSS_SELECTOR,".newstext p")])
+    content = driver.find_element(By.CSS_SELECTOR,".newstext div").text.replace('\n', ' ')
+    
+
     return title,content,time
 
 
-def crwal_one_stock(stock_id, base_url = "https://guba.eastmoney.com/list,{0},1,f_{1}.html"):
-    global start_date, end_date
+def crwal_one_stock(stock_id,start_date,end_date,base_url = "https://guba.eastmoney.com/list,{0},1,f_{1}.html"):
     driver = webdriver.Chrome(options=chrome_options)
     first_message_time = datetime.now()
     pages = 1
@@ -48,11 +50,12 @@ def crwal_one_stock(stock_id, base_url = "https://guba.eastmoney.com/list,{0},1,
             tot_items += hrefitems
         pages += 1
         
+
     res = []
     for href in tot_items:
         title,content,time = get_news_details(driver,href)
         if start_date<= datetime.strptime(time, '%Y-%m-%d %H:%M:%S') <= end_date:
-            res.append(time,title,content)
+            res.append((time, title, content))
         if datetime.strptime(time, '%Y-%m-%d %H:%M:%S') < start_date:
             break
 
@@ -60,15 +63,16 @@ def crwal_one_stock(stock_id, base_url = "https://guba.eastmoney.com/list,{0},1,
     driver.quit()
     return res
 
-def crawl_stocks_and_save(stock_id_list):
+def crawl_stocks_and_save(news_base,start_date,end_date,stock_id_list):
     for stock_id in stock_id_list:
         news_path = os.path.join(news_base, f"{stock_id}.csv")
         
         if os.path.exists(news_path):
             print("! 股票 {} 已采集过".format(stock_id))
             continue
-
-        res = crwal_one_stock(stock_id)
+        print("股票 {} 采集中".format(stock_id))
+        res = crwal_one_stock(stock_id,start_date,end_date)
+        print("股票 {} 采集完成，共{}条记录。".format(stock_id,len(res)))
 
         # 创建 DataFrame
         df = pd.DataFrame(res, columns=["Time", "Title", "Content"])
@@ -76,12 +80,11 @@ def crawl_stocks_and_save(stock_id_list):
 
 
 if __name__=="__main__":
-    start_date, end_date = datetime(2017, 1, 1), datetime(2020, 1, 1)
-    stock_codes = ["000001"]
-    news_base = "data/stock_news"
-    os.makedirs(news_base, exist_ok=True)
-    NUM_PROCS = 10
+    start_date, end_date = datetime(2024, 6, 1), datetime(2024, 6, 15)
+    stock_codes = ["000001","000002","000003","000004"]
+    news_base = "stock_news"
+    os.makedirs(news_base,exist_ok=True)
+    NUM_PROCS = 2
     pool = ProcessPool(NUM_PROCS)
     split_codes = np.array_split(stock_codes, NUM_PROCS)
-    pool.map(crawl_stocks_and_save, split_codes)
-
+    pool.map(crawl_stocks_and_save,[news_base]*NUM_PROCS, [start_date]*NUM_PROCS, [end_date]*NUM_PROCS, split_codes)
